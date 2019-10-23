a2c42223c824873f847665706d93efab3d13decc
yapruixuancharlotte <52947040+yapruixuancharlotte@users.noreply.github.com>
2019-10-23 13:13:33 +0800

    Charlotte 1J

--- t01primeshcflcm_hcf4.py
+++ t01primeshcflcm_hcf4.py
@@ -1,4 +1,12 @@
 # Find the HCF of n positive integers.
 
-n = 
+n = if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i
+            
+    return hcf
 
